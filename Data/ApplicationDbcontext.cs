85e9c33531bff4120b300c5e60b9eb4590c26425
DatLwu <datluu351@gmail.com>
2024-01-06 13:59:03 +0700

    LTD174_Cau2

--- Data/ApplicationDbcontext.cs
+++ Data/ApplicationDbcontext.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using LuuTienDat_174.Models;
 
 namespace LuuTienDat_174.Data{
     public class ApplicationDbContext : DbContext{
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+        public DbSet<SinhVien> SinhVien {get; set;}
     }
 }
---
+++ Models/SinhVien.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LuuTienDat_174.Models{
+    [Table("SinhViens")]
+    public class SinhVien{
+        [Key]
+        public string MaSinhVien { get; set; }
+        public int SoBaoDanh { get; set; }
+        public double Diem { get; set; }
+    }
+}
